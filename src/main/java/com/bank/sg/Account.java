c259976c3c43e0371aaebd5416b8ac0ea302f97b
eric PAUWAWE <eric.pauwawe@gmail.com>
2020-11-24 15:44:51 +0100

    case of withdrawal

--- src/main/java/com/bank/sg/Account.java
+++ src/main/java/com/bank/sg/Account.java
@@ -16,6 +16,17 @@ public class Account {
         balance = balance + amount;
     }
 
+
+    public void makeWithdrawal(double amount) throws OperationAccountException {
+        checkAmount(amount);
+        if (amount <= balance) {
+            this.balance = balance - amount;
+        } else {
+            throw new OperationAccountException("insufficient funds to realize the operation");
+        }
+
+    }
+
     private void checkAmount(double amount) throws OperationAccountException {
         if (amount <= 0) {
             throw new OperationAccountException("amount not supported by the operation");
--- src/test/java/com/bank/sg/AccountTest.java
+++ src/test/java/com/bank/sg/AccountTest.java
@@ -3,6 +3,9 @@ package com.bank.sg;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
 public class AccountTest {
 
 
@@ -20,17 +23,74 @@ public class AccountTest {
     }
 
 
-    @Test(expected = OperationAccountException.class)
+    @Test
     public void should_not_make_deposit() throws OperationAccountException {
         //given
         Account account = new Account(500);
         double amount = -1000;
 
         //when
-        account.makeDeposit(amount);
+        Throwable thrown = catchThrowable(() -> {
+            account.makeDeposit(amount);
+        });
 
         //then
-        Assertions.assertThat(account.getBalance()).isEqualTo(500);
+        assertThat(thrown).isInstanceOf(OperationAccountException.class);
+        assertThat(thrown).hasMessageContaining("amount not supported");
+        assertThat(account.getBalance()).isEqualTo(500);
+
+    }
+
+    @Test
+    public void should_make_withdrawal() throws OperationAccountException {
+        //given
+        Account account = new Account(1000);
+        double amount = 300;
+
+
+        //when
+        account.makeWithdrawal(amount);
+
+        //then
+        assertThat(account.getBalance()).isEqualTo(700);
+    }
+
+    @Test
+    public void should_not_make_withdrawal_because_insuffisient_funds() {
+        //given
+        Account account = new Account();
+        double amount = 300;
+
+
+        //when
+        Throwable thrown = catchThrowable(() -> {
+            account.makeWithdrawal(amount);
+        });
+
+
+        //then
+        assertThat(thrown).isInstanceOf(OperationAccountException.class);
+        assertThat(thrown).hasMessageContaining("insufficient funds");
+        assertThat(account.getBalance()).isEqualTo(0);
+    }
+
+    @Test
+    public void should_not_make_withdrawal_because_negative_amount() {
+        //given
+        Account account = new Account(1000);
+        double amount = -300;
+
+
+        //when
+        Throwable thrown = catchThrowable(() -> {
+            account.makeWithdrawal(amount);
+        });
+
+
+        //then
+        assertThat(thrown).isInstanceOf(OperationAccountException.class);
+        assertThat(thrown).hasMessageContaining("amount not supported");
+        assertThat(account.getBalance()).isEqualTo(1000);
     }
 
 
